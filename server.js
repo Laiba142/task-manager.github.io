13721e8a337c2c5b906ff56ecdd8ecc8d8d811bd
Laiba142 <laibazahir2003@gmail.com>
2024-09-20 17:54:40 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+let tasks = [];
+let taskId = 1;
+
+
+app.use(express.static('public'));
+
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks); 
+});
+
+
+app.post('/tasks', (req, res) => {
+  res.json(task); 
+});
+
+
+app.put('/tasks/:id', (req, res) => {
+  const id = parseInt(req.params.id); 
+  const task = tasks.find(t => t.id === id); 
+
+  if (task) {
+    res.json(task); 
+  } else {
+    res.status(404).send('Task not found'); 
+  }
+});
+
+
+app.delete('/tasks/:id', (req, res) => {
+  const id = parseInt(req.params.id); 
+  tasks = tasks.filter(t => t.id !== id); 
+  res.status(204).send(); 
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
